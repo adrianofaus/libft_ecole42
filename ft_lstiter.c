676e403e63bc8dda189a3054c1a5a3255a5c5af8
Adriano Faustino <faustino.adr@gmail.com>
2022-03-13 20:55:22 -0300

    Fixed all norminette errors

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,13 +6,13 @@
 /*   By: afaustin <afaustin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 17:04:53 by afaustin          #+#    #+#             */
-/*   Updated: 2021/10/19 17:04:54 by afaustin         ###   ########.fr       */
+/*   Updated: 2022/03/13 20:52:21 by afaustin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstiter(t_list *lst, void(*f)(void *))
+void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
 	if (!lst)
 		return ;
